a364fb221b5eeba0b4e8f077affd322b6b80e8d6
Harshana Chathuranga <91200650+wmhchathuranga@users.noreply.github.com>
2023-03-11 07:17:54 +0530

    while loop2

--- datadog setup.sh
+++ datadog setup.sh
@@ -144,7 +144,7 @@ GRANT EXECUTE ON PROCEDURE datadog.enable_events_statements_consumers TO datadog
 "
 
   read -p "Can we Proceed [Y/n]?" go
-  while [ $go -ne "Y" ]; do
+  while [ $go != "Y" ]; do
     read -p "Can we Proceed [Y/n]?" go
   done
   cp /etc/datadog-agent/conf.d/mysql.d/conf.yaml.example /etc/datadog-agent/conf.d/mysql.d/conf.yaml
